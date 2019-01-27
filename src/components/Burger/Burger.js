6839ca84db12f65da12ff147a33a46a8d17e3e65
Constantinos Stathis <constantinos-@hotmail.com>
2019-01-27 23:23:07 +0200

    120. Outputting Burger Ingredients Automatically

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,13 +3,21 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => (
-  <div className={classes.Burger}>
-    <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
-    <BurgerIngredient type="bread-bottom" />
-  </div>
-);
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+      .map(ingredientKey => {
+        return [...Array(props.ingredients[ingredientKey])]
+            .map((_,i) => (<BurgerIngredient key={ingredientKey + i} type={ingredientKey} />
+        ));
+      });
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="bread-top" />
+      {transformedIngredients}
+      <BurgerIngredient type="bread-bottom" />
+    </div>
+  );
+}
 
 export default burger;
