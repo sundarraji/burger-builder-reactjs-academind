6b40ff08c2fb5e5bcbb0022f0fd5e138b4f9c31c
Constantinos Stathis <constantinos-@hotmail.com>
2019-07-18 14:18:15 +0300

    176. Reusing the Backdrop

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,11 +3,18 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
-const sideDrawer = () => {
-  // ...
+const sideDrawer = ( props ) => {
+  const attachedClasses = [
+    classes.SideDrawer,
+    (props.open) ? classes.Open : classes.Close
+  ];
+
   return (
-    <div className={classes.SideDrawer}>
+    <>
+    <Backdrop show={props.open} clicked={props.closed}/>
+    <div className={attachedClasses.join(' ')}>
       <div className={classes.Logo}>
         <Logo />
       </div>
@@ -15,6 +22,7 @@ const sideDrawer = () => {
         <NavigationItems />
       </nav>
     </div>
+    </>
   );
 };
 
