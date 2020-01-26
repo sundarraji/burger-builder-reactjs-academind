ce965e539200a04fb6f04cc07506406cf556a692
Constantinos Stathis <constantinos-@hotmail.com>
2020-01-26 16:07:57 +0200

    213. Handling Errors

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -130,4 +131,4 @@ class BurgerBuilder extends Component {
   )};
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
